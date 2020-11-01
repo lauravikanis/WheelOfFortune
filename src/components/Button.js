e50081295fbd8793e9e9c537ccc26c603da78508
bwnstk <info@flimmerkiste.xyz>
2020-11-01 19:05:57 +0100

    Add focus styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function Button({ className, onClick, innerText }) {
   return (
-    <button className={`button glow-on-hover ${className}`} onClick={onClick}>
+    <button
+      tabIndex={"0"}
+      className={`button glow-on-hover ${className}`}
+      onClick={onClick}
+    >
       {innerText}
     </button>
   );
