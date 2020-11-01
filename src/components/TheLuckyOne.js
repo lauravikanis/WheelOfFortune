a1d1385b4870cae7f34aecae7fd3f2705399f44a
bwnstk <info@flimmerkiste.xyz>
2020-11-01 16:56:06 +0100

    :art: add margin left/right to values

--- src/components/TheLuckyOne.js
+++ src/components/TheLuckyOne.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function TheLuckyOne({ theLuckyOne }) {
   return (
-    <div className="theLuckyOne__container">
+    <div className="theLuckyOne__container unMarkable">
       <h3 className="theLuckyOne__itsYou">🚀 Its you! 👩‍🎤</h3>
       <h2 className="theLuckyOne__text  glow">✨ {theLuckyOne} ✨</h2>
     </div>
